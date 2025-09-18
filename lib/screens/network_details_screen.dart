72a10c6d4b4b92eda806822e0a5945142502b751
TawhibAbdulrab7 <tawheebalmahdi@gmail.com>
2025-09-18 03:46:54 +0300

    إضافة AppBar وخلفية لشاشة NetworkDetails

--- lib/screens/network_details_screen.dart
+++ lib/screens/network_details_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class NetworkDetailsScreen extends StatelessWidget {
-  const NetworkDetailsScreen({super.key});
+  final String ssid;
+  final String strength;
+  final String type;
+
+  NetworkDetailsScreen({
+    required this.ssid,
+    required this.strength,
+    required this.type,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("تفاصيل الشبكه")),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple.shade200, Colors.deepPurple.shade300],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+      ),
+    );
   }
 }
