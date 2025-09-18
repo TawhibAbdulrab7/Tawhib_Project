d46e30e756d233b76a81670f699c6deb1fb05649
TawhibAbdulrab7 <tawheebalmahdi@gmail.com>
2025-09-18 03:03:17 +0300

    إضافة قائمة الشبكات لشاشة Home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+  final List<Map<String, String>> networks = [
+    {'ssid': "Hamza Net", 'strength': '4/5', 'type': 'WPA2'},
+    {'ssid': 'Mohammed Net', 'strength': '3/5', 'type': 'WPA3'},
+    {'ssid': "ALbarq Net", 'strength': '5/5', 'type': 'WPA2'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("قائمة الشبكات"),),
+      appBar: AppBar(title: Text("قائمة الشبكات")),
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.blue.shade200,Colors.purple.shade300],
-          begin: Alignment.topLeft,
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade200, Colors.purple.shade300],
+            begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
         ),
+        child: ListView.builder(
+          itemCount: networks.length,
+            itemBuilder: (context, index) {
+            final net = networks[index];
+            return Card(
+              margin: EdgeInsets.all(10),
+              child: ListTile(
+                leading: Icon(Icons.wifi,color: Colors.blue,),
+                title: Text(net['ssid']!),
+                subtitle: Text('قوة الإشارة: ${net['strength']} - نوع: ${net['type']}'),
+              ),
+            );
+
+            }),
       ),
     );
   }
