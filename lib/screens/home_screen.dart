e768a1255fc59cbeb46f9370c778f77e64e733f2
TawhibAbdulrab7 <tawheebalmahdi@gmail.com>
2025-09-21 06:18:12 +0300

    تكرار بعض الشبكات من اجل التنسيق الواجهه

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,14 +8,14 @@ class HomeScreen extends StatelessWidget {
     {'ssid': '20_AlHamza_Net', 'strength': '4/5', 'type': 'WPA2'},
     {'ssid': "15_AlHamza_Net", 'strength': '4/5', 'type': 'WPA2'},
     {'ssid': "18_AlHamza_Net", 'strength': '4/5', 'type': 'WPA2'},
-    // {'ssid': 'AlMohammed_Net_10', 'strength': '3/5', 'type': 'WPA3'},
-    // {'ssid': "AlMohammed_Net_7", 'strength': '3/5', 'type': 'WPA3'},
-    // {'ssid': "AlMohammed_Net_11", 'strength': '3/5', 'type': 'WPA3'},
-    // {'ssid': 'AlMohammed_Net_13', 'strength': '3/5', 'type': 'WPA3'},
-    // {'ssid': "ALbarq_Net_53", 'strength': '5/5', 'type': 'WPA2'},
-    // {'ssid': "ALbarq_Net_57", 'strength': '5/5', 'type': 'WPA2'},
-    // {'ssid': 'ALbarq_Net_79', 'strength': '5/5', 'type': 'WPA2'},
-    // {'ssid': "ALbarq_Net_80", 'strength': '5/5', 'type': 'WPA2'},
+    {'ssid': 'AlMohammed_Net_10', 'strength': '3/5', 'type': 'WPA3'},
+    {'ssid': "AlMohammed_Net_7", 'strength': '3/5', 'type': 'WPA3'},
+    {'ssid': "AlMohammed_Net_11", 'strength': '3/5', 'type': 'WPA3'},
+    {'ssid': 'AlMohammed_Net_13', 'strength': '3/5', 'type': 'WPA3'},
+    {'ssid': "ALbarq_Net_53", 'strength': '5/5', 'type': 'WPA2'},
+    {'ssid': "ALbarq_Net_57", 'strength': '5/5', 'type': 'WPA2'},
+    {'ssid': 'ALbarq_Net_79', 'strength': '5/5', 'type': 'WPA2'},
+    {'ssid': "ALbarq_Net_80", 'strength': '5/5', 'type': 'WPA2'},
   ];
 
   @override
